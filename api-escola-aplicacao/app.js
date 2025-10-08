caf2fc3df990c026d608279c5777ca7c1c6ca181
Draconic-Hacker <pythontesteemail55@gmail.com>
2025-10-08 09:30:57 -0300

    criação de um formulário html para o metodo post na api || criação do arquivo db.json para iniciar a conexão com o mysql || atualização dos métodos na api no app.js

--- api-escola-aplicacao/app.js
+++ api-escola-aplicacao/app.js
@@ -1,24 +1,37 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const port = 3000;
+//middleware para envio de dados json
+app.use(express.json());
+//middleware para envio e recebimento de dados simples de formulario
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World my api</h1>")
-})
-
-app.get('/users', (req, res) => {
-    res.send("listando usuario")
-})
-
-app.get('/users/:id', (req, res) => {
-    res.send("usuario novo")
-})
-
-app.post('/users', (req, res) => {
-    res.send("esse é o post")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World my api</h1>");
+});
+//metodo get para o endpoint /users
+app.get("/users", (req, res) => {
+  res.send("Listando usuarios");
+});
+//metodo get para o endpoint /users com parametro
+app.get("/users/:id", (req, res) => {
+  res.send("Usuario novo");
+});
+//metodo post para o endpoint /users
+app.post("/users", (req, res) => {
+  console.log("Dados JSON recebidos:", req.body);
+  //res.send(`Dados JSON processados: ${JSON.stringify(req.body)}`)
+  res.status(200).json(req.body);
+});
+//metodo put para o endpoint /users
+app.put("/users/:id", (req, res) => {
+  res.send("Esse é o put");
+});
+//metodo delete para o endpoint /users
+app.delete("/users/:id", (req, res) => {
+  res.send("Esse é o delete");
+});
 
 app.listen(port, () => {
-    console.log("Servidor rodando na porta ", port);
-})
+  console.log("Servidor rodando na porta ", port);
+});
